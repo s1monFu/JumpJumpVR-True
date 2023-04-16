606f72729bccd237aaec65647794430bcebe1cf6
s1monFu <laissezfu@gmail.com>
2023-04-16 13:40:07 -0500

    New Scene and Jump

---
+++ Assets/scripts/PlayerMoveBack.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveBack : MonoBehaviour
+{
+    public float speed = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+
+    void Update()
+    {
+        if (transform.position.z > -10)
+        {
+            transform.Translate(0, 0, -speed * Time.deltaTime);
+        }
+
+    }
+}
