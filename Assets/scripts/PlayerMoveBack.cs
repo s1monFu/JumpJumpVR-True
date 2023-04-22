00de47f15a5afadc097ceff550b0cec057596288
Justin Qiao <71228724+ShichenQiao@users.noreply.github.com>
2023-04-22 18:20:52 -0500

    fixed win scene

--- Assets/scripts/PlayerMoveBack.cs
+++ Assets/scripts/PlayerMoveBack.cs
@@ -6,6 +6,7 @@ public class PlayerMoveBack : MonoBehaviour
 {
     public float speed = 0.5f;
     public GameOverUI gameOverUI;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,7 +23,7 @@ public class PlayerMoveBack : MonoBehaviour
             transform.Translate(0, 0, -speed * Time.deltaTime);
         }
         else if( transform.position.z < 42f ){
-            gameOverUI.ShowGameOverPanel();
+            //UnityEngine.SceneManager.LoadScene("Demo");
         }
 
     }
