59b0782fd7bebc2056270fd0cdd0fb31a9639962
Justin Qiao <71228724+ShichenQiao@users.noreply.github.com>
2023-04-22 18:29:39 -0500

    Added win scene

--- Assets/scripts/PlayerMoveBack.cs
+++ Assets/scripts/PlayerMoveBack.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMoveBack : MonoBehaviour
 {
     public float speed = 0.5f;
-    public GameOverUI gameOverUI;
+    //public string scene_name;
 
     // Start is called before the first frame update
     void Start()
@@ -13,17 +14,15 @@ public class PlayerMoveBack : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-
-
     void Update()
     {
         if (transform.position.z > -6f && transform.position.z < 42f)
         {
             transform.Translate(0, 0, -speed * Time.deltaTime);
         }
-        else if( transform.position.z < 42f ){
-            //UnityEngine.SceneManager.LoadScene("Demo");
+
+        if( transform.position.z >= 40f ){
+            SceneManager.LoadScene("YOU_WIN");
         }
 
     }
